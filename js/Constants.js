a783d747e00ce0a1c814c25707f6ef58f63c9348
Theodoros Stefanidis <stefanidhes@gmail.com>
2024-07-02 20:13:52 +0300

    Add files via upload

---
+++ js/Constants.js
@@ -0,0 +1,273 @@
+LANDS = ['144960','144961','144962','144704','144705','144706','144449','144450'];
+
+CONTINENT = [61];
+
+PERCENTAGE = parseFloat(0.65);
+
+WALLETS = [
+    {
+        "CONTINENT": "61",
+        "kingdomId": "61ee518e83e3e11712ea9af2",
+        "wallet": "0xc3a1BA029e020Db0594669d61658E1f7Ab178Eb3",
+        "name": "Acidve"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "629c81d89e690b6d3ee5fbc3",
+        "wallet": "0xc3a1BA029e020Db0594669d61658E1f7Ab178Eb3",
+        "name": "Acidve's"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "643431dab8521e1aec621731",
+        "wallet": "0xc3a1BA029e020Db0594669d61658E1f7Ab178Eb3",
+        "name": "Acidve *"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "653f4ab02d360f46a1e8a401",
+        "wallet": "0x74A5F48baF120f10bc822D8D7554BC59872A6e70",
+        "name": "Ildrial"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "654be74c3c826e6888658fe8",
+        "wallet": "0x9a8A8e4D828e5B35bfF58E94e753611A2eAB2059",
+        "name": "Orlany "
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "61e120eb11c7431144959462",
+        "wallet": "0xd51c1623f36676c6105f6a4d2b8402f08de28e99",
+        "name": "King"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "654231dd46dbc3462938f4f5",
+        "wallet": "0x2F8A6054f2ff1629a8f773977597A2f731853A5c",
+        "name": "Zigorat"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "655cf0cac2f5983f12640dc4",
+        "wallet": "0xcF8292122444FE57EF02169eE6CA140bcB4c6506 ",
+        "name": "Cozmo "
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "653f859e7217f6348a440770",
+        "wallet": "0x314741c3dcdaa62f76cc535cf3d3db7c2e62b7c7",
+        "name": "Noctis"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "65482add83916d692cccbef8",
+        "wallet": "0x9abf5f3aa27252e4b4566dd0a8fbfbda6491d753",
+        "name": "Fight club"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "655860d57947a7108bb1741c",
+        "wallet": "0x5b4bee6dba2f77cc2ba577bfbd948ce2615be9cf",
+        "name": "Zals "
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "618e8387e364ca46156710ce",
+        "wallet": "0xef605a1BDE6F9a54cc057abEEd3f696307f14409",
+        "name": "AlexMag"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "655a359b987c50664a8c171c",
+        "wallet": "0xA2EF7d30768dAe0ce1a466B8E527cfCa29EB7218",
+        "name": "RAINE_UP"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "6577215785004327e59058f5",
+        "wallet": "0xcCd660C85d198eB86585014eCFDae75d5c22674A",
+        "name": "Jerry "
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "64f34a9f4c1f084ab2f10288",
+        "wallet": "0xabebd74d1a63e6952b59ee936410760e30892522",
+        "name": "Hernan Salim "
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "6311248d7cbaf503fb118bdd",
+        "wallet": "0x3Fd492C975808D0d9c52E128072987b3545B1962",
+        "name": "FM"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "6561d0e315096817c557745e",
+        "wallet": "0xD34C69B15647ECB611CbA05CFf81Ea1e55C5e79d",
+        "name": "C.M.U."
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "655cdc24b30e0045cca15b67",
+        "wallet": "0xe3B88054409F9C1642Ccca694d86703379c7DA98",
+        "name": "Drgos2"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "6542919737611a2995a181c5",
+        "wallet": "0xc5c09e462e5943375e9ace95217542729fd0d037",
+        "name": "Evil.live"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "61e90029fc919e229adf2a42",
+        "wallet": "0x721A07Ac2A7b7731e97E86279017E93a9077E7f1",
+        "name": "Quydain"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "",
+        "wallet": "0x0aa480d09a2301364d6acefaea1d5ca57028e933",
+        "name": "Estilorock"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "654b70171329f94fbf590a65",
+        "wallet": "0xc642d18ae1149349132fbaed9d35dd787af2230c",
+        "name": "Oili ri (tanx)"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "628a3545a082c16e7f82aec6",
+        "wallet": "0x2B4062dEb4659EC0f488c3533f9e8aD43b5E4C9d",
+        "name": "Floripa"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "654d1abc598416129d959422",
+        "wallet": "0x2B4062dEb4659EC0f488c3533f9e8aD43b5E4C9d",
+        "name": "Floripa_Br"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "63d82444b6f2a33059c59fa9",
+        "wallet": "0x2B4062dEb4659EC0f488c3533f9e8aD43b5E4C9d",
+        "name": "Gui_Floripa"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "63c89f47b35bed35259a219b",
+        "wallet": "0x2B4062dEb4659EC0f488c3533f9e8aD43b5E4C9d",
+        "name": "Gui_Boy_Br"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "62d46f6ae573c718c4369a63",
+        "wallet": "0x2B4062dEb4659EC0f488c3533f9e8aD43b5E4C9d",
+        "name": "Boy_Floripa_Br"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "655c66d219a5fb1cb90929de",
+        "wallet": "0x0ec29E7e4Cc5C3bAAdD6E09bAd040b6905Cb1EF5",
+        "name": "Urboss"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "6499be31e8bf0b0474ac76d4",
+        "wallet": "0xc546F71881D6FcdE5b6d4d1417aA27e16cB569F7",
+        "name": "AyzAk"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "6462546fad78890a9ad796b4",
+        "wallet": "0xdDaf4f9a2640FFF312C2B8BB037228c2463F1c0C",
+        "name": "Saed_Knani"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "650d038e9e42121b3143bf8a",
+        "wallet": "0x9eF4b952f9C06F794346887e4159eadf2FA6A3Fc",
+        "name": "Storm"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "614b04ac183c8a409e4e4d0b",
+        "wallet": "0x5F33e6E1221d38b9bA7b9b52F9A800A9eF873667",
+        "name": "lotte"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "618b27c7a62e9f7da896ffca",
+        "wallet": "0xbff3ed84EaDd31eB760b23633Fb9DE1A7D3C11B9",
+        "name": "Vua Gà Mạng 123"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "63d96abac6a8d63a56339ec0",
+        "wallet": "0x451db604532345d0b8c714465b16c5b2f1969d81",
+        "name": "Gécko"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "65138741ed6236730ad2d128",
+        "wallet": "0xeB9aC56f3158d921a9E964598a376855D64B0D45",
+        "name": "Hedge"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "653f628a68ef6331f986a911",
+        "wallet": "0x7eb0b11de9b1c6bc726addaba331d921c8033044",
+        "name": "KALEGİBİKALE"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "62a36588a9e20f029ee33749",
+        "wallet": "0x7F85544A3C37bfc052b95587905c802dD80d9339",
+        "name": "☯ ПΣMΣƧIƧ_UP ☯"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "657baee42094d2159c7e6d8a",
+        "wallet": "0x5b242815ea33b40a07fab1b2a223316b72610e8a",
+        "name": "ULAN ™"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "6214c6e045334248c52b2e41",
+        "wallet": "0xe91b53d999b08dbdf40e170206ab840cb1c9cc11",
+        "name": "Beniwal"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "6485ab912852f44a25e97570",
+        "wallet": "0xc888b14d91B1c6001D53e221778849d300d1DCA1",
+        "name": "꧁ Espanuy ꧂"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "65571f0ab8f95f191c953d0a",
+        "wallet": "0xc888b14d91B1c6001D53e221778849d300d1DCA1",
+        "name": "꧁ Espanuy 2 ꧂"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "656633fa749ba17d7b5555df",
+        "wallet": "0xc888b14d91B1c6001D53e221778849d300d1DCA1",
+        "name": "꧁ Espanuy 3 ꧂"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "6564a486d5967a7f60035f64",
+        "wallet": "0x90b95287088d503d27fa3d41e5e1a6bbacd907bd",
+        "name": "Dragon.mix □"
+    },
+    {
+        "CONTINENT": "61",
+        "kingdomId": "660a76af38cf5eda8992bfcc",
+        "wallet": "0x88CAa81eC7A5e57a60BB36a795B20e5A9D40E929",
+        "name": "XtraRicE"
+    }
+];
+
